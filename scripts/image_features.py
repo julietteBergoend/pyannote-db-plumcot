ce51c368639fa2fdf0e80a0f7a0f3ce67c8cbffc
paullerner <1.paul.lerner@gmail.com>
2019-11-28 17:06:13 +0100

    feat: compute character references by doing a clustering over all images , works way better !

--- scripts/image_features.py
+++ scripts/image_features.py
@@ -19,6 +19,12 @@ import imageio
 from pyannote.video import Face
 from pyannote.video.utils.scale_frame import scale_up_bbox, rectangle_to_bbox
 
+## clustering
+from pyannote.core.utils.distance import cdist,pdist
+from scipy.spatial.distance import squareform
+from scipy.cluster.hierarchy import fcluster
+from pyannote.core.utils.hierarchy import linkage,fcluster_auto
+
 def extract_image(rgb,landmarks_model,embedding_model,output,
                  return_landmarks=False,return_embedding=False):
     """Facial features detection for an rgb image
@@ -77,7 +83,7 @@ def compute_features(image_jsons,MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_
     for i,image_json in enumerate(image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['allImages']):
         print((
             f"\rimage {i+1}/{image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['totalImageCount']}."
-        ),end="")
+        ),end="                    ")
         image_path=image_json.get("path")
         if image_path is not None:
             image_path=image_path[0]
@@ -121,7 +127,7 @@ def compute_features(image_jsons,MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_
         f"and {no_image} were never scraped because of a lack of labelling."
     ))
     return image_jsons
-    
+
 def compute_reference(character,t=0.6,method='complete',KEEP_IMAGE_TYPES=None,keep_faces=False):
     """
     Cluster over features then save the biggest cluster as reference.
@@ -190,8 +196,111 @@ def compute_reference(character,t=0.6,method='complete',KEEP_IMAGE_TYPES=None,ke
     if keep_faces:
         return references,faces
     return references
+def compute_references(image_jsons,t=0.6,method='complete',KEEP_IMAGE_TYPES=None,keep_faces=False):
+    """
+    Clusters over every image in image_jsons
+    then assigns to every cluster the most recurring label in the caption
+    Starts with the biggest clusters first
+    Parameters:
+    -----------
+    image_jsons: dict
+        described in `CONTRIBUTING.md`, it contains the path towards precomputed features for every character
+    t: float, str, optional
+        Threshold to apply when forming flat clusters.
+        If 'auto' (case-sensitive) then we use pyannote.core.utils.hierarchy.fcluster_auto
+            to automatically determine the threshold
+        Defaults to 0.6 because of dlib (see https://github.com/davisking/dlib-models)
+    method: str, optional
+        Method used to calculate the distance between the
+        newly formed cluster :math:`u` and each :math:`v`
+        see scipy.cluster.hierarchy.linkage
+    KEEP_IMAGE_TYPES: set, optional
+        Restricts the cluster to features which were computed on a given imageType (e.g. 'still_frame')
+        See `CONTRIBUTING.md`
+        Defaults to keep all features (i.e. None)
+    keep_faces: bool, optional
+        keep track of rgb image of faces (cropped with the bounding box)
+        for debugging and visualization.
+        Defaults to False.
+    Returns:
+    --------
+    image_jsons: dict
+        updated database with the path towards the reference embedding
+    """
+    features=[]
+    save_labels=[]
+    if keep_faces:
+        faces=[]
 
-def compute_references(image_jsons,t=0.6,method='complete',MIN_IMAGES=1,KEEP_IMAGE_TYPES=None):
+    #Clusters over every image in image_jsons
+    for i,image in enumerate(image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['allImages']):
+        print((
+                f"\rimage {i+1}/{image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['totalImageCount']}."
+            ),end="                    ")
+        if 'features' not in image:
+            continue
+        if KEEP_IMAGE_TYPES is not None:
+            if image['imageType'] not in KEEP_IMAGE_TYPES:
+                continue
+        if keep_faces:
+            rgb=imageio.imread(image['path'][0])
+            frame_height=rgb.shape[0]
+            frame_width=rgb.shape[1]
+        for feature in np.load(image['features'][0]):#this way we skip those that are empty (because no (frontal) face was detected)
+            features.append(feature["embeddings"])
+            save_labels.append(image['label'])
+            if keep_faces:
+                left, top, right, bottom=scale_up_bbox(feature["bbox"],frame_width,frame_height)
+                faces.append(rgb[top:bottom,left:right])
+    features=np.vstack(features)
+    #clustering
+    Z=linkage(features,method=method, metric='euclidean')
+    if t == 'auto':
+        clustering=fcluster_auto(features,Z, metric='euclidean')
+    else:
+        clustering=fcluster(Z,t,criterion='distance')
+    unique, counts = np.unique(clustering, return_counts=True)
+
+    #assigns to every cluster the most recurring label in the caption
+    assigned_labels=[]
+    unassigned_clusters=[]
+    sorted_counts=np.sort(np.unique(counts))[::-1]
+    keep_centroid=[]
+
+    for count in sorted_counts:
+        for cluster in np.where(counts==count)[0]:#start with the biggest clusters
+            cluster_i=np.where(clustering==unique[cluster])[0]#get the indexes of the cluster
+            cluster_labels=np.array(save_labels)[cluster_i]#get the labels associated to the cluster
+            #flatten the labels
+            flat_cluster_labels = np.array([label for labels in cluster_labels for label in labels])
+            unique_labels, count_labels = np.unique(flat_cluster_labels, return_counts=True)
+            #assign the most reccuring label to the cluster
+            cluster_label=unique_labels[np.argmax(count_labels)]
+            #except if we already assigned it to a bigger cluster
+            if cluster_label in assigned_labels:
+                unassigned_clusters.append(cluster)
+                continue
+
+            #save reference and update image_jsons
+            str_KEEP_IMAGE_TYPES = ".".join(KEEP_IMAGE_TYPES) if KEEP_IMAGE_TYPES is not None else str(KEEP_IMAGE_TYPES)
+            output_path=os.path.join(IMAGE_PATH,cluster_label,f'{str_KEEP_IMAGE_TYPES}.{MODEL_NAME}.{cluster_label}.{method}.{t}.references.npy')
+            np.save(output_path,features[cluster_i])
+            if "references" in image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['characters'][cluster_label]:
+                image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['characters'][cluster_label]["references"].append(output_path)
+            else:
+                image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['characters'][cluster_label]["references"]=[output_path]
+            assigned_labels.append(cluster_label)
+            if keep_faces:
+                distance_from_cluster=np.mean(squareform(pdist(features[cluster_i],metric='euclidean')),axis=0)
+                centroid_face=faces[cluster_i[np.argmin(distance_from_cluster)]]
+                keep_centroid.append(centroid_face)
+    print(f"assigned {len(assigned_labels)} labels over {len(unique)} clusters")
+    print(f"those cluster were not assigned any label :\n{unassigned_clusters}")
+    if keep_faces:
+        return image_jsons, faces
+    return image_jsons
+
+def compute_references_per_character(image_jsons,t=0.6,method='complete',MIN_IMAGES=1,KEEP_IMAGE_TYPES=None):
     """
     Cluster over each character folder if it has at least `MIN_IMAGES` images features in it
         then save the biggest cluster as the character reference.
@@ -223,9 +332,10 @@ def compute_references(image_jsons,t=0.6,method='complete',MIN_IMAGES=1,KEEP_IMA
     image_jsons: dict
         updated database with the path towards the reference embedding
     """
+    warnings.warn("This function has been deprecated in favor of compute_references")
     n_characters=len(image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['characters'])
     for i,(name,character) in enumerate(image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['characters'].items()):
-        print(f"processing {name} ({i}/{n_characters})",end="\r")
+        print(f"\rprocessing {name} ({i}/{n_characters})",end="                           ")
         #using len(character['features']) instead of characer['count']
         #  as some images do not contain frontal face or are grayscale
         if 'features' in character and len(character['features'])>=MIN_IMAGES:
@@ -238,18 +348,19 @@ def compute_references(image_jsons,t=0.6,method='complete',MIN_IMAGES=1,KEEP_IMA
             else:
                 image_jsons['mediaviewer']['galleries'][SERIE_IMDB_ID]['characters'][name]["references"]=[output_path]
     return image_jsons
+
 def main(image_jsons,MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID,IMAGE_PATH,
-    CLUSTERING_THRESHOLD,CLUSTERING_METHOD):
+    CLUSTERING_THRESHOLD,CLUSTERING_METHOD,KEEP_IMAGE_TYPES):
     image_jsons=compute_features(image_jsons,MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID)
-    image_jsons=compute_references(image_jsons,CLUSTERING_THRESHOLD,CLUSTERING_METHOD,MIN_IMAGES,KEEP_IMAGE_TYPES)
+    image_jsons=compute_references(image_jsons,CLUSTERING_THRESHOLD,CLUSTERING_METHOD,KEEP_IMAGE_TYPES)
     with open(os.path.join(IMAGE_PATH,"images.json"),"w") as file:
         json.dump(image_jsons,file)
-    print("\ndone computing features ;)")
+    print("\ndone computing features and references ;)")
 
 if __name__ == '__main__':
     #raise NotImplementedError()
-    from images import MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID,IMAGE_PATH,CLUSTERING_THRESHOLD,CLUSTERING_METHOD
+    from images import MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID,IMAGE_PATH,CLUSTERING_THRESHOLD,CLUSTERING_METHOD,KEEP_IMAGE_TYPES
     with open(os.path.join(IMAGE_PATH,"images.json"),"r") as file:
         image_jsons=json.load(file)
     main(image_jsons,MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID,IMAGE_PATH,
-        CLUSTERING_THRESHOLD,CLUSTERING_METHOD)
+        CLUSTERING_THRESHOLD,CLUSTERING_METHOD,KEEP_IMAGE_TYPES)
