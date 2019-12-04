474e3b229e97d1325ef1b230bfca613231f86075
paullerner <1.paul.lerner@gmail.com>
2019-12-04 10:52:25 +0100

    feat: scrap all series (given series.txt)

--- scripts/images.py
+++ scripts/images.py
@@ -6,7 +6,10 @@ image_scraping: Get images from IMDB character's page given a list of urls and c
     (following the format of `characters.txt` described in CONTRIBUTING.md)
 image_features: Then extracts features off theses images.
 """
+
 # Dependencies
+import json
+import numpy as np
 import os
 from image_scraping import main as scrap
 from image_features import main as compute_references
@@ -18,31 +21,31 @@ SEPARATOR=","
 DATA_PATH=os.path.join("Plumcot","data")
 with open(os.path.join(DATA_PATH,"series.txt")) as file:
     series=file.readlines()
-SERIE_URI,_,SERIE_IMDB_URL,_,_=series[5].split(",")
-CHARACTERS_PATH=os.path.join(DATA_PATH,SERIE_URI,'characters.txt')
-IMAGE_PATH=os.path.join(DATA_PATH,SERIE_URI,'images')
 IMAGE_FORMAT="jpg"
 
 ## web
 IMDB_URL="https://www.imdb.com"
 THUMBNAIL_CLASS="titlecharacters-image-grid__thumbnail-link"
 IMAGE_CLASS="pswp__img"
-SERIE_IMDB_ID=SERIE_IMDB_URL.split("/")[-2]
 
 ## ML
 MODEL_NAME="dlib_face_recognition_resnet_model_v1"
-DLIB_EMBEDDING=f"dlib-models/{MODEL_NAME}.dat"
-DLIB_LANDMARKS="dlib-models/shape_predictor_68_face_landmarks.dat"
+DLIB_MODELS="/people/lerner/pyannote/pyannote-video/dlib-models"
+DLIB_EMBEDDING=os.path.join(DLIB_MODELS,f"{MODEL_NAME}.dat")
+DLIB_LANDMARKS=os.path.join(DLIB_MODELS,"shape_predictor_68_face_landmarks.dat")
 DLIB_THRESHOLD=0.6#threshold for clustering, see https://github.com/davisking/dlib-models
 MIN_IMAGES=5
 EMBEDDING_DIM=128
 EMBEDDING_DTYPE=('embeddings', 'float64', (EMBEDDING_DIM,))
 BBOX_DTYPE=('bbox', 'float64', (4,))
+CLUSTERING_THRESHOLD=DLIB_THRESHOLD#'auto'
+CLUSTERING_METHOD='complete'
+KEEP_IMAGE_TYPES={'still_frame'}
 
-def main(SERIE_URI,SERIE_IMDB_URL,IMAGE_PATH,N_COL,SEPARATOR,
-    MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID):
-    image_jsons=scrap(SERIE_URI,SERIE_IMDB_URL,IMAGE_PATH,N_COL,SEPARATOR)
-    image_jsons=compute_references(image_jsons,MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID)
+def main(SERIE_URI,SERIE_IMDB_URL,IMAGE_PATH,CHARACTERS_PATH,N_COL,SEPARATOR,IMAGE_FORMAT,
+    MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING):
+    image_jsons=scrap(SERIE_URI,SERIE_IMDB_URL,IMAGE_PATH,CHARACTERS_PATH,N_COL,SEPARATOR,IMAGE_FORMAT)
+    #image_jsons=compute_references(image_jsons,MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING)
     with open(os.path.join(IMAGE_PATH,"images.json"),"w") as file:
         json.dump(image_jsons,file)
     print("\ndone ;)")
@@ -53,5 +56,14 @@ if __name__ == '__main__':
     # with open(os.path.join(IMAGE_PATH,"images.json"),"r") as file:
     #     image_jsons=json.load(file)
     # query_image_from_json(image_jsons,IMAGE_PATH,actor2character,SEPARATOR)
-    main(SERIE_URI,SERIE_IMDB_URL,IMAGE_PATH,N_COL,SEPARATOR,
-        MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING,SERIE_IMDB_ID)
+    for serie in series:
+        SERIE_URI,_,SERIE_IMDB_URL,_,_=serie.split(",")
+        if SERIE_IMDB_URL=='':#film -> open episode:
+            SERIE_IMDB_URL=np.loadtxt(os.path.join(DATA_PATH,SERIE_URI,'episodes.txt'),dtype=str,delimiter=SEPARATOR)[:,2]
+        else:#only one but turn it to a list so we can iterate it
+            SERIE_IMDB_URL=[SERIE_IMDB_URL]
+        CHARACTERS_PATH=os.path.join(DATA_PATH,SERIE_URI,'characters.txt')
+        IMAGE_PATH=os.path.join(DATA_PATH,SERIE_URI,'images')
+
+        main(SERIE_URI,SERIE_IMDB_URL,IMAGE_PATH,CHARACTERS_PATH,N_COL,SEPARATOR,IMAGE_FORMAT,
+            MODEL_NAME,DLIB_LANDMARKS,DLIB_EMBEDDING)
