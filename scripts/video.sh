bf0efe95564e751ab3cc3cf93eb38a4f6c9c5dfb
Paul Lerner <1.paul.lerner@gmail.com>
2019-12-05 16:21:06 +0100

    refactor: more readable echos

--- scripts/video.sh
+++ scripts/video.sh
@@ -10,15 +10,15 @@
 echo "This is task ${SGE_TASK_ID} of job ${JOB_ID}."
 export ffmpeg=/people/bredin/cluster/bin/ffmpeg
 export file_uri=`head -n ${SGE_TASK_ID} $1 | tail -n 1`
-echo $file_uri
+echo file_uri: $file_uri
 export MODEL_NAME=dlib_face_recognition_resnet_model_v1
 export DLIB_MODELS=/people/lerner/pyannote/pyannote-video/dlib-models
 export DLIB_EMBEDDING=${DLIB_MODELS}/${MODEL_NAME}.dat
 export DLIB_LANDMARKS=${DLIB_MODELS}/shape_predictor_68_face_landmarks.dat
-echo ${MODEL_NAME}
-echo ${DLIB_MODELS}
-echo ${DLIB_EMBEDDING}
-echo ${DLIB_LANDMARKS}
+echo MODEL_NAME: ${MODEL_NAME}
+echo DLIB_MODELS: ${DLIB_MODELS}
+echo DLIB_EMBEDDING: ${DLIB_EMBEDDING}
+echo DLIB_LANDMARKS: ${DLIB_LANDMARKS}
 export VIDEOS_PATH=/vol/work3/maurice/dvd_extracted
 export DATA_PATH=/vol/work/lerner/pyannote-db-plumcot
 export SERIE_URI=$2
@@ -26,9 +26,9 @@ export SERIE_PATH=${DATA_PATH}/Plumcot/data/${SERIE_URI}
 export video_path=${VIDEOS_PATH}/${SERIE_URI}/$file_uri.mkv
 export shots_path=${SERIE_PATH}/video/${file_uri}.shots.json
 export features_path=${SERIE_PATH}/video/${file_uri}.npy
-echo ${video_path}
-echo ${shots_path}
-echo ${features_path}
+echo video_path: ${video_path}
+echo shots_path: ${shots_path}
+echo features_path: ${features_path}
 `which pyannote-structure.py` shot --verbose --ffmpeg=${ffmpeg} ${video_path} \
                                                                 ${shots_path}
 `which pyannote-face.py` track --verbose --every=0.0 --ffmpeg=${ffmpeg} ${video_path} \
--- scripts/video_per_season.sh
+++ scripts/video_per_season.sh
@@ -1,4 +1,4 @@
-echo ${PWD}
+echo PWD: ${PWD}
 VIDEOS_PATH='/vol/work3/maurice/dvd_extracted'
 SERIE_URI='Friends'
 export DATA_PATH=/vol/work/lerner/pyannote-db-plumcot
@@ -9,19 +9,19 @@ export MODEL_NAME=dlib_face_recognition_resnet_model_v1
 export DLIB_MODELS=/people/lerner/pyannote/pyannote-video/dlib-models
 export DLIB_EMBEDDING=${DLIB_MODELS}/${MODEL_NAME}.dat
 export DLIB_LANDMARKS=${DLIB_MODELS}/shape_predictor_68_face_landmarks.dat
-echo ${MODEL_NAME}
-echo ${DLIB_MODELS}
-echo ${DLIB_EMBEDDING}
-echo ${DLIB_LANDMARKS}
+echo MODEL_NAME: ${MODEL_NAME}
+echo DLIB_MODELS: ${DLIB_MODELS}
+echo DLIB_EMBEDDING: ${DLIB_EMBEDDING}
+echo DLIB_LANDMARKS: ${DLIB_LANDMARKS}
 for video_path in ${VIDEOS_PATH}/${SERIE_URI}/${SERIE_URI}.Season${SEASON_NUMBER}*.mkv; do
   file_name=`basename "${video_path}"`
   extension="${file_name##*.}"
   file_uri="${file_name%.*}"
   shots_path=${SERIE_PATH}/video/${file_uri}.shots.json
   features_path=${SERIE_PATH}/video/${file_uri}.npy
-  echo ${video_path}
-  echo ${shots_path}
-  echo ${features_path}
+  echo video_path: ${video_path}
+  echo shots_path: ${shots_path}
+  echo features_path: ${features_path}
   # pyannote-structure.py shot --verbose ${video_path} \
   #                                      ${shots_path}
   # pyannote-face.py track --verbose --every=0.0 ${video_path} \
