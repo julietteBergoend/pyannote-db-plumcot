44ce9ca8bbe62a0e60a46eb2f4549e3ceda90188
Paul Lerner <1.paul.lerner@gmail.com>
2019-12-09 15:46:41 +0100

    feat: main script for multimodal fusion

--- scripts/multimodal.py
+++ scripts/multimodal.py
@@ -3,11 +3,27 @@
 """
 Fuses outputs of pyannote.audio and pyannote.video models
 """
+# Dependencies
+
+import os
+
 from pyannote.database.util import load_rttm
 from pyannote.video.face.clustering import FaceClustering
 from pyannote.metrics.diarization import DiarizationErrorRate
 
-def fuse(video_features_path,audio_hypothesis_path,file_uri,mode='intersection'):
+#hyper parameters
+
+SERIE_URI="Friends"
+DATABASE=f"Plumcot-{SERIE_URI}"
+TASK="SpeakerDiarization"
+PROTOCOL="UEM"
+SET="test"
+DATA_PATH=os.path.join("Plumcot","data")
+OUTPUT_PATH=os.path.join(DATA_PATH,SERIE_URI,"multimodal")
+VIDEO_FEATURES=os.path.join(DATA_PATH,SERIE_URI,"video")
+AUDIO_HYPOTHESIS_PATH=f"/vol/work/lerner/baseline/dia/Plumcot-Friends-Adversarial/der_uem/train/Plumcot-Friends.SpeakerDiarization.UEM.development/{DATABASE}.{TASK}.{PROTOCOL}.{SET}.rttm"
+
+def fuse(video_features_path,audio_hypothesis,file_uri,mode='intersection'):
     """
     Fuses outputs of pyannote.audio and pyannote.video models
 
@@ -15,8 +31,8 @@ def fuse(video_features_path,audio_hypothesis_path,file_uri,mode='intersection')
     -----------
     video_features_path: str
         Path to the video features (.npy) file as defined in pyannote.video
-    audio_hypothesis_path: str
-        Path to the diarization output (.rttm) file as defined in pyannote.audio
+    audio_hypothesis: Annotation
+        hypothesis made by the audio model
     file_uri: str
         uri of the file you're interested in (used to filter out audio_hypothesis)
     mode: str
@@ -25,8 +41,6 @@ def fuse(video_features_path,audio_hypothesis_path,file_uri,mode='intersection')
     clustering = FaceClustering()
     #TODO : move the preprocess (i.e. npy to pyannote) to some other place ?
     face_id, _ = clustering.model.preprocess(video_features_path)
-    
-    audio_hypothesis=load_rttm(audio_hypothesis_path)[file_uri]
 
     optimal_mapping=DiarizationErrorRate().optimal_mapping(face_id, audio_hypothesis)
     audio_hypothesis=audio_hypothesis.rename_labels(mapping=optimal_mapping)
@@ -34,3 +48,23 @@ def fuse(video_features_path,audio_hypothesis_path,file_uri,mode='intersection')
     fusion=audio_hypothesis.crop(face_id,mode=mode)
 
     return fusion
+
+def main(audio_hypothesis_path,video_features,output_path,
+    DATABASE,TASK,PROTOCOL,SET):
+    """
+    audio_hypothesis_path: str
+        Path to the diarization output (.rttm) file as defined in pyannote.audio
+    """
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    audio_hypotheses=load_rttm(audio_hypothesis_path)
+    for uri,audio_hypothesis in audio_hypotheses.items():
+        print(f"Fusing {uri}",end='\r')
+        video_features_path=os.path.join(video_features,f"{uri}.npy")
+        fusion=fuse(video_features_path,audio_hypothesis,uri)
+        with open(os.path.join(output_path,f'{DATABASE}.{TASK}.{PROTOCOL}.{SET}.rttm'),'a') as file:
+            fusion.write_rttm(file)
+            
+if __name__=="__main__":
+    main(AUDIO_HYPOTHESIS_PATH,VIDEO_FEATURES,OUTPUT_PATH,
+        DATABASE,TASK,PROTOCOL,SET)
