2d3dc266ed7356eb7bde30c4a50ada7e5dc3b3ea
paullerner <1.paul.lerner@gmail.com>
2020-01-03 15:22:02 +0100

    feat: (ad-hoc): split double episodes (marked with b) and increment followings

--- scripts/transcripts.py
+++ scripts/transcripts.py
@@ -17,31 +17,71 @@ with open(os.path.join(SERIE_PATH,"episodes.txt"),'r') as file:
         episodes=file.read().split("\n")
         episodes=set([episode.split(',')[0] for episode in episodes])
 correct=False
-old_season_number=None
-for transcript in sorted(os.listdir(transcript_path)):
-    uri,extension=os.path.splitext(transcript)
-    season_number=int(re.findall(r'\d+', uri.split(".")[1])[0])
-    if season_number!=old_season_number:
-        old_season_number=season_number
-        print("\n",season_number)
-        decrement=0
-    if uri.split(".")[-1].isdigit():
 
-        new_uri=".".join(uri.split(".")[:-1])
-        episode_number=int(new_uri.split(".")[-1][-2:])-decrement
-        new_uri=f'{".".join(new_uri.split(".")[:-1])}.Episode{episode_number:02d}'
-        #print(uri)
-        decrement+=1
-    else:
-        episode_number=int(uri.split(".")[-1][-2:])-decrement
-        new_uri=f'{".".join(uri.split(".")[:-1])}.Episode{episode_number:02d}'
-    if uri != new_uri:
-        print(f"{uri} -> {new_uri}")
+
+def get_season_number(uri):
+    return int(re.findall(r'\d+', uri.split(".")[1])[0])
+
+def increment_episode_number(uri,increment):
+    episode_number=int(uri.split(".")[-1][-2:])+increment
+    new_uri=f'{".".join(uri.split(".")[:-1])}.Episode{episode_number:02d}'
+    return new_uri
+
+def Increment(transcript_path):
+    old_season_number=None
+    todo={}
+    for transcript in sorted(os.listdir(transcript_path)):
+        uri,extension=os.path.splitext(transcript)
+        if extension!=".txt" and extension != ".temp":
+            continue
+        season_number=get_season_number(uri)
+        if season_number!=old_season_number:
+            old_season_number=season_number
+            print("\n",season_number)
+            increment=0
+        if uri[-1]=="b":
+            new_uri=uri[:-1]
+            increment+=1
+            new_uri=increment_episode_number(new_uri,increment)
+        else:
+            new_uri=increment_episode_number(uri,increment)
+        if uri != new_uri:
+            print(f"{uri} -> {new_uri}")
+            todo[os.path.join(transcript_path,transcript)]=os.path.join(transcript_path,f"{new_uri}{extension}")
+
+    for key, file_dir in sorted(todo.items(), key=lambda x:x[0], reverse=True):
+        print(key,file_dir)
         if correct:
             os.rename(
-                os.path.join(transcript_path,transcript),
-                os.path.join(transcript_path,f"{new_uri}{extension}")
+                key,
+                file_dir
             )
+    return todo
+
+Increment(transcript_path)
+def Decrement(transcript_path):
+    old_season_number=None
+    for transcript in sorted(os.listdir(transcript_path)):
+        uri,extension=os.path.splitext(transcript)
+        season_number=get_season_number(uri)
+        if season_number!=old_season_number:
+            old_season_number=season_number
+            print("\n",season_number)
+            decrement=0
+        if uri.split(".")[-1].isdigit():
+            new_uri=".".join(uri.split(".")[:-1])
+            new_uri=increment_episode_number(new_uri,decrement)
+            #print(uri)
+            decrement-=1
+        else:
+            new_uri=increment_episode_number(uri,decrement)
+        if uri != new_uri:
+            print(f"{uri} -> {new_uri}")
+            if correct:
+                os.rename(
+                    os.path.join(transcript_path,transcript),
+                    os.path.join(transcript_path,f"{new_uri}{extension}")
+                )
     # if uri not in episodes:
     #     print(uri)
 
