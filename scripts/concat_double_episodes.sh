556212991f412777f4797814c195905a929a57d9
paullerner <1.paul.lerner@gmail.com>
2020-01-14 12:42:40 +0100

    feat: concat double episodes en16kHz.wav using ffmpeg

---
+++ scripts/concat_double_episodes.sh
@@ -0,0 +1,13 @@
+export SERIE_URI=$1
+export WAV_PATH=/vol/work3/lefevre/dvd_extracted
+export EXTENSION=en16kHz.wav
+#/vol/work3/lefevre/dvd_extracted/TheOffice/TheOffice.Season04.Episode03.04.en16kHz.wav
+for file in Plumcot/data/$SERIE_URI/double_episodes/*.txt; do
+	echo file: $file
+	file_name=`basename "${file}"`
+	file_uri="${file_name%.*}"
+#	echo file_uri: $file_uri
+	output=$WAV_PATH/$file_uri.$EXTENSION
+	echo output: $output
+	ffmpeg -f concat -safe 0 -i $file -c copy $output
+done
