a6572fe54007f5d8bfa2b8aae2fb6414710376be
Paul Lerner <lerner@tuta.io>
2020-03-17 12:21:18 +0100

    feat: concat_double_episodes: customize wav_path

--- scripts/concat_double_episodes.sh
+++ scripts/concat_double_episodes.sh
@@ -1,12 +1,10 @@
 export SERIE_URI=$1
-export WAV_PATH=/vol/work3/lefevre/dvd_extracted
+export WAV_PATH=$2
 export EXTENSION=en16kHz.wav
-#/vol/work3/lefevre/dvd_extracted/TheOffice/TheOffice.Season04.Episode03.04.en16kHz.wav
 for file in Plumcot/data/$SERIE_URI/double_episodes/*.txt; do
 	echo file: $file
 	file_name=`basename "${file}"`
 	file_uri="${file_name%.*}"
-#	echo file_uri: $file_uri
 	output=$WAV_PATH/$1/$file_uri.$EXTENSION
 	echo output: $output
 	ffmpeg -f concat -safe 0 -i $file -c copy $output
