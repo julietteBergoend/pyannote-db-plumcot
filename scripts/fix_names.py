469a185313a3a48ca7e133444773c4cba6dc6966
Paul Lerner <lerner@tuta.io>
2020-08-10 18:34:35 +0200

    feat: fix_names: fix character names after 123e37cb8b64bb6a9bdd89ed665a2769d992569f

--- scripts/fix_names.py
+++ scripts/fix_names.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import Plumcot as PC
 import json
+import re
+import warnings
 
 DATA_PATH = Path(PC.__file__).parent / 'data'
 
 
-def diff_to_json():
+def diff_to_json(write=True):
     with open(DATA_PATH/'characters.diff') as file:
         diff = file.read().split('\n')
 
@@ -36,8 +38,36 @@ def diff_to_json():
             a, b = [], []
         else:
             continue
+    if write:
+        for uri, serie in per_serie.items():
+            with open(DATA_PATH/uri/'characters.diff.json', 'w') as file:
+                json.dump(serie, file, sort_keys=True, indent=4)
+    else:
+        return per_serie
 
-    for name, serie in per_serie.items():
-        with open(DATA_PATH/name/'characters.diff.json', 'w') as file:
-            json.dump(serie, file, sort_keys=True, indent=4)
 
+def fix_names(per_serie):
+    for uri, serie in per_serie.items():
+        if uri != 'Friends':
+            continue
+        serie_path = DATA_PATH/uri
+        for data in serie_path.iterdir():
+            if not data.is_dir():
+                continue
+            for file_path in data.iterdir():
+                if not file_path.is_file():
+                    continue
+                print(file_path)
+                with open(file_path) as file:
+                    try:
+                        old = file.read()
+                    except UnicodeDecodeError as e:
+                        warnings.warn(f"Skipping '{file_path} because of a "
+                                      f"UnicodeDecodeError:\n{e}'")
+                for old_name, new_name in serie.items():
+                    old = re.sub(rf'\b{old_name}\b', new_name, old)
+                with open(file_path, 'w') as file:
+                    file.write(old)
+
+
+fix_names(diff_to_json(False))
